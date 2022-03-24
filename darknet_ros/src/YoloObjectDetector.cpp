96c63477c40d300ba554b45fbd4e0072a7ae2017
t1mkhuan9 <timkhuang@icloud.com>
2022-03-24 23:48:39 +0000

    fix to show result image

--- darknet_ros/src/YoloObjectDetector.cpp
+++ darknet_ros/src/YoloObjectDetector.cpp
@@ -9,11 +9,10 @@
 // yolo object detector
 #include "darknet_ros/YoloObjectDetector.hpp"
 
-// fill_cpu 
-#include "../../darknet/src/blas.h"
-
 // Check for xServer
 #include <X11/Xlib.h>
+
+#include "../../darknet/src/blas.h"
 
 #ifdef DARKNET_FILE_PATH
 std::string darknetFilePath_ = DARKNET_FILE_PATH;
@@ -165,18 +164,7 @@ void YoloObjectDetector::cameraCallback(const sensor_msgs::ImageConstPtr& msg) {
   cv_bridge::CvImagePtr cam_image;
 
   try {
-    if (msg->encoding == "mono8" || msg->encoding == "bgr8" || msg->encoding == "rgb8") {
-      cam_image = cv_bridge::toCvCopy(msg, msg->encoding);
-    } else if ( msg->encoding == "bgra8") {
-      cam_image = cv_bridge::toCvCopy(msg, "bgr8");
-    } else if ( msg->encoding == "rgba8") {
-      cam_image = cv_bridge::toCvCopy(msg, "rgb8");
-    } else if ( msg->encoding == "mono16") {
-      ROS_WARN_ONCE("Converting mono16 images to mono8");
-      cam_image = cv_bridge::toCvCopy(msg, "mono8");
-    } else {
-      ROS_ERROR("Image message encoding provided is not mono8, mono16, bgr8, bgra8, rgb8 or rgba8.");
-    }
+    cam_image = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
   } catch (cv_bridge::Exception& e) {
     ROS_ERROR("cv_bridge exception: %s", e.what());
     return;
@@ -394,23 +382,23 @@ void* YoloObjectDetector::fetchInThread() {
 }
 
 void* YoloObjectDetector::displayInThread(void* ptr) {
-  show_image_cv(buff_[(buffIndex_ + 1) % 3], "YOLO");
-  // int c = show_image(buff_[(buffIndex_ + 1) % 3], "YOLO", 1);
-  // if (c != -1) c = c % 256;
-  // if (c == 27) {
-  //   demoDone_ = 1;
-  //   return 0;
-  // } else if (c == 82) {
-  //   demoThresh_ += .02;
-  // } else if (c == 84) {
-  //   demoThresh_ -= .02;
-  //   if (demoThresh_ <= .02) demoThresh_ = .02;
-  // } else if (c == 83) {
-  //   demoHier_ += .02;
-  // } else if (c == 81) {
-  //   demoHier_ -= .02;
-  //   if (demoHier_ <= .0) demoHier_ = .0;
-  // }
+  show_image_cv(buff_[(buffIndex_ + 1)%3], "YOLO V4");
+  int c = cv::waitKey(waitKeyDelay_);
+  if (c != -1) c = c % 256;
+  if (c == 27) {
+    demoDone_ = 1;
+    return 0;
+  } else if (c == 82) {
+    demoThresh_ += .02;
+  } else if (c == 84) {
+    demoThresh_ -= .02;
+    if (demoThresh_ <= .02) demoThresh_ = .02;
+  } else if (c == 83) {
+    demoHier_ += .02;
+  } else if (c == 81) {
+    demoHier_ -= .02;
+    if (demoHier_ <= .0) demoHier_ = .0;
+  }
   return 0;
 }
 
@@ -438,7 +426,7 @@ void YoloObjectDetector::setupNetwork(char* cfgfile, char* weightfile, char* dat
   demoThresh_ = thresh;
   demoHier_ = hier;
   fullScreen_ = fullscreen;
-  printf("YOLO\n");
+  printf("YOLO V4\n");
   net_ = load_network(cfgfile, weightfile, 0);
   set_batch_network(net_, 1);
 }
@@ -486,12 +474,12 @@ void YoloObjectDetector::yolo() {
 
   int count = 0;
   if (!demoPrefix_ && viewImage_) {
-    cv::namedWindow("YOLO", cv::WINDOW_NORMAL);
+    cv::namedWindow("YOLO V4", cv::WINDOW_NORMAL);
     if (fullScreen_) {
-      cv::setWindowProperty("YOLO", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
+      cv::setWindowProperty("YOLO V4", cv::WND_PROP_FULLSCREEN, cv::WINDOW_FULLSCREEN);
     } else {
-      cv::moveWindow("YOLO", 0, 0);
-      cv::resizeWindow("YOLO", 640, 480);
+      cv::moveWindow("YOLO V4", 0, 0);
+      cv::resizeWindow("YOLO V4", 640, 480);
     }
   }
 
